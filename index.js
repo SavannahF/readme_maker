3a4d088f930b745c38b9addad32faccebc5c6340
Savannah Fortson <67602838+SavannahF@users.noreply.github.com>
2021-04-17 23:13:51 -0400

    index.js structure

--- index.js
+++ index.js
@@ -1,3 +1,36 @@
-// some input variables will go up here
+// Input variables/modules will go up here
 
-// Array of Questions
+// Array of Questions, prompted for input
+// GitHub username
+// email
+// Project Title
+// Project Description
+// Installation steps
+// Project Notes or other instruction
+// Instructions
+// License Options--list *done*
+// Contributors
+
+const questions = [
+  {
+    type: "input",
+    name: "",
+    message: "",
+  },
+  {
+    type: "input",
+    name: "",
+    message: "",
+  },
+  // one will be list for license choices
+  {
+    type: "list",
+    name: "",
+    message: "",
+    choices: ["Apache", "IBM", "MIT", "Perl"],
+  },
+];
+
+// Function to create new README file skeleton, awaiting input
+
+// Function to run program, take input, show success or error
