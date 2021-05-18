c1eb1dae8375e8c7c35f56d10fa1c1a2fa3278d8
Savannah Fortson <savannah_hc@savannah-hc.attlocal.net>
2021-05-17 21:33:35 -0400

    restructure utils

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
+// TODO: Include packages needed for this application
+
+// TODO: Create an array of questions for user input
+const questions = [];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
+
+// END OF DEVELOP INDEX
+
+
 // Input variables/modules will go up here
 
 // Array of Questions, prompted for input
