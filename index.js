284c1533c4165c82857fdf30e6bc7baf4e5687c1
Savannah Fortson <savannah_hc@savannah-hc.attlocal.net>
2021-05-17 22:05:24 -0400

    index functions updated

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
-// TODO: Include packages needed for this application
+// Include packages needed for this application
+// Inquirer, fs, generateMarkdown
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
 const questions = [];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// function to write README file
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, function (error) {
+    if (error) {
+        return console.log(error);
+    }
+    console.log(`${fileName} has been successfully created!`);
+})
+}
 
-// TODO: Create a function to initialize app
-function init() {}
+// function to initialize app
+function init() {
+    inquirer.prompt(questions)
+        .then(function(data) {
+          writeToFile("newREADME.md", generatorMarkdown(data))})
+        .then(() => console.log("README successfully written."))
+        .catch((err) => console.log(err))
+}
 
 // Function call to initialize app
 init();
