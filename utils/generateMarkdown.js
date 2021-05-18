98e7c0c5626bd6ae333c8faa2ba3f469aa5158ea
Savannah Fortson <savannah_hc@savannah-hc.attlocal.net>
2021-05-17 22:15:59 -0400

    generateMarkdown updates

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,4 +1,5 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
+
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {}
 
@@ -10,10 +11,46 @@ function renderLicenseLink(license) {}
 // If there is no license, return an empty string
 function renderLicenseSection(license) {}
 
-// TODO: Create a function to generate markdown for README
+//function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  ## Table of Contents
+  * [Description](#Description)
 
+  * [Installation](#Installation-Instructions)
+
+  * [Usage Instructions](#Usage-Instructions)
+
+  * [License](#License)
+
+  * [Contributing Members](#Contributing-Members)
+
+  * [Testing](#Testing) 
+    
+  * [Questions](#Questions)
+
+  ## Description
+  ${data.description}
+
+  ## Installation Instructions 
+  ${data.installation}
+
+  ## Usage Instructions
+  ${data.usage}
+
+  ## License
+  ${data.license}
+
+  ## Contributing Members
+  ${data.contributors}
+
+  ## Testing 
+   ${data.test}
+
+  ## Questions
+
+  I am open for questions and feedback on this repository, please contact me directly at ${data.email}. 
+  For other projects I've worked on, see [${data.github}](https://github.com/${data.github}/).
 `;
 }
 
