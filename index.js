0fa1ad7e6f458b089090664ebb7a7ff3c8979745
Savannah Fortson <savannah_hc@savannah-hc.attlocal.net>
2021-05-17 22:33:07 -0400

    questions updated

--- index.js
+++ index.js
@@ -5,7 +5,54 @@ const fs = require("fs");
 const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'Title',
+        message: 'What is the title for your project?',
+    },
+    {
+        type: 'input',
+        name: 'Description',
+        message: 'Please provide a description of your project:',
+    },
+    {
+        type: 'input',
+        name: 'Installation',
+        message: 'What necessary dependencies must be installed to run this app?',
+    },
+    {
+        type: 'input',
+        name: 'Usage',
+        message: 'Please provide the usage instructions:',
+    },
+    {
+        type: 'list',
+        name: 'License',
+        choices: ['Apache License 2.0', ' MIT License', 'Eclipse Public License 2.0', 'Mozilla Public License 2.0'],
+        message: 'Please select the type of licese:',
+    },
+    {
+        type: 'input',
+        name: 'Contributors',
+        message: 'Please provide any contributing members.'
+    },
+    {
+        type: 'input',
+        name: 'Test',
+        message: 'Please provide testing instructions for this application.'
+    },
+    {
+        type: 'input',
+        name: 'GitHub',
+        message: 'What is your GitHub username?',
+    },
+    {
+        type: 'input',
+        name: 'Email',
+        message: "Which email would you like to provide (will be used for inquiries?)",
+    }
+];
 
 // function to write README file
 function writeToFile(fileName, data) {
